9c03c1c374e6b7f8aafb9a00a70c92a564ecac40
Alex Victoor <alexvictoor@gmail.com>
2020-04-16 18:45:14 +0200

    auto deploy

--- src.77de5100.js
+++ src.77de5100.js
@@ -921,7 +921,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60687" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61264" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -1098,4 +1098,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel/src/builtins/hmr-runtime.js","index.ts"], null)
-//# sourceMappingURL=/src.77de5100.js.map
+//# sourceMappingURL=src.77de5100.js.map
