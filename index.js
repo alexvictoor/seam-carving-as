351cb36191763281cda949d64783c28a1f9d4c8f
Alex Victoor <alexvictoor@gmail.com>
2020-04-19 14:11:36 +0200

    auto deploy

--- index.js
+++ index.js
@@ -212,7 +212,11 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+<<<<<<< HEAD
   var ws = new WebSocket(protocol + '://' + hostname + ':' + "59712" + '/');
+=======
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49456" + '/');
+>>>>>>> auto deploy
 
   ws.onmessage = function (event) {
     checkedAssets = {};
