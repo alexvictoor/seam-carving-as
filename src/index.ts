b3714306ff7db220ab3c2fa9800caef535d7b491
Alex Victoor <alexvictoor@gmail.com>
2020-04-19 14:11:16 +0200

    fix non chrome browsers with an OffscreenCanvas polyfill

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
 import loader from "@assemblyscript/loader";
 
-console.log(loader);
+//
+// OffscreenCanvas polyfill
+// https://gist.github.com/n1ru4l/9c7eff52fe084d67ff15ae6b0af5f171
+//
+if (!window.OffscreenCanvas) {
+  (window as any).OffscreenCanvas = class OffscreenCanvas {
+    canvas: any;
+    constructor(width, height) {
+      this.canvas = document.createElement("canvas");
+      this.canvas.width = width;
+      this.canvas.height = height;
+
+      this.canvas.convertToBlob = () => {
+        return new Promise((resolve) => {
+          this.canvas.toBlob(resolve);
+        });
+      };
+
+      return this.canvas;
+    }
+  };
+}
 
 const initWasm = async () => {
   const initial = 1;
